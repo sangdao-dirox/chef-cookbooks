5d7bac197c625a2897e9bf8da5822d00543eb271
Minh Sang <sangdm@dirox.net>
2015-09-01 14:08:25 +0700

    test

--- symfony/recipes/default.rb
+++ symfony/recipes/default.rb
@@ -1,13 +1,5 @@
-#Init dependency
+###Deploy
 #
-#
-
-%w{mcrypt php5-mcrypt php5-xsl}.each do |p|
-    package p do
-        action :install
-    end
-end
-
 node[:deploy].each do |application, deploy|
     #create bin and vendor directory
     [ "#{deploy[:deploy_to]}/current/bin", "#{deploy[:deploy_to]}/current/vendor" ].each do |path|
@@ -17,6 +9,7 @@ node[:deploy].each do |application, deploy|
             group 'root'
             recursive true
             action :create
+            not_if { ::File.exists?(path) }
         end
     end
 
@@ -31,13 +24,50 @@ node[:deploy].each do |application, deploy|
         EOH
     end
 
-    # Post deploy commands (such as Asset Installs / Assetic)
-    node[:post_deploy_symfony_commands].each do |cmd|
+    # Symfony configuration 
+    template "#{deploy[:deploy_to]}/current/app/config/parameters.yml" do
+        source "parameters.yml.erb"
+        mode 0644
+        user "apache"
+        group "apache"
+        variables({
+            :locale => deploy[:locale] || "en"
+            :database_host => deploy[:database][:host] || "127.0.0.1",
+            :database_port => deploy[:database][:port] || "null",
+            :database_user => deploy[:database][:username] || "root",
+            :database_password => deploy[:database][:password] || "evasion_pass",
+            :database_database => deploy[:database][:database] || "evasion_main",
+            :mailer_transport => deploy[:mail][:transport] || "smtp",
+            :mailer_host => deploy[:mail][:host] || "127.0.0.1",
+            :mailer_user => deploy[:mail][:username] || "null",
+            :mailer_password => deploy[:mail][:password] || "null",
+            :parameters => node[:custom_env], 
+            :application => "#{application}",
+            :secret => SecureRandom.base64 
+        })
+    end
+
+    #enable write mode for cache, logs
+    execute "run_allow_write_on_cache_and_logs" do
+        command "chmod -R 777 app/cache app/logs"
+        cwd "#{deploy[:deploy_to]}/current"
+        user "root"
+        group "root"
+    end
+    # Post deploy commands
+    deploy[:post_deploy_symfony_commands].each do |cmd|
         execute "run_symfony_console_#{cmd}" do
             command "php app/console #{cmd} --env=prod"
             cwd "#{deploy[:deploy_to]}/current"
             user "apache"
             group "apache"
         end
+    end
+    #enable write mode for cache, logs
+    execute "run_allow_write_on_cache_and_logs" do
+        command "chmod -R 777 app/cache app/logs"
+        cwd "#{deploy[:deploy_to]}/current"
+        user "root"
+        group "root"
     end
 end
---
+++ symfony/recipes/setup.rb
@@ -0,0 +1,16 @@
+#Init dependency
+#
+#
+
+%w{mcrypt php5-mcrypt php5-xsl}.each do |p|
+    package p do
+        action :install
+    end
+end
+
+#Enable the 
+execute "enable php5enmod mcrypt" do
+    command "php5enmod mcrypt"
+    user "root"
+    group "root"
+end
